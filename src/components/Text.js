f40c07ebcaa336286f3f9ed59584d7996797835d
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-02-29 23:10:55 +0100

    added new files

---
+++ src/components/Text.js
@@ -0,0 +1,12 @@
+export const about = `My name is Joshua Oseghale. I am a software engineer proficient in building
+web / mobile applications. I am very passionate about creating lasting solutions that not only
+meet the client's need but also appeals to the user, giving them a lasting experience.`
+
+export const skills = `My expertise extends beyond the basics, as I am well-versed in popular
+frameworks like React, allowing me to build dynamic and interactive user experiences.
+I have skill in UI/UX design principles as this help me follow best design practices in order
+to give a pleasing user experience.
+Beyond web/ app development, I have well rounded knowledge in the fundamentals of software
+engineering which is an essential skill helpful in accomplishing complex programming tasks `
+
+export const projects_text = `Below are some of my projects... I Would keep on updating this section`
