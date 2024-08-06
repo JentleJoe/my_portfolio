cc1157df0b40d0fd1e0cd41ee7ea193d0921319a
Joshua Oseghale <110992018+JentleJoe@users.noreply.github.com>
2024-08-06 14:01:19 +0100

    Linked email to connect btn

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -58,7 +58,7 @@ export default function Banner(){
                         {/* <span className="tagline" >Welcome to my Portfolio</span> */}
                         <h1>{ 'Hi I\'m JentleJoe, ' }<span className="wrap"> {text} </span> </h1>
                         <p className="about-text">I am a <span className="real-title">Frontend Web Developer</span>{about}</p>
-                        <button className="connect-btn">Contact Me <ArrowRightCircle size={25} /></button>
+                        <button className="connect-btn" onClick={() => window.location.href = 'mailto:joshuaoseghale01@gmail.com'}>Contact Me <ArrowRightCircle size={25} /></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={astronaut} alt="Header Img" />
