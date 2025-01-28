9ee9248ed4ef0f658ba302949d19fafd1552cab9
JentleJoe <aigbokhaebholojoshua@gmail.com>
2025-01-28 07:22:25 +0100

    removed comment

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,6 @@ import {ArrowRightCircle} from 'react-bootstrap-icons'
 export default function ProjectCard({title, description, imgUrl, projUrl, redirect, inProgress}){
 
     return(
-        // CREATE A NEW CARD COMPONENT AND ADD THE RESPECTIVE STYLES
         <Col sm={6} md={4}>
             <div className="proj-imgbx">
                 <img className="proj-img" src={imgUrl} alt="image" />
